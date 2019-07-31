712a4e72fe20ff6c1c84289ba2a8341ee9ba831d
Breezy <breezyvanpatten042@gmail.com>
2019-07-31 15:56:17 -0400

    WEBSITE STUFF

---
+++ myjs.js
@@ -0,0 +1,9 @@
+//add an alert box for page to test connection
+alert("Welcome to my JavaScript. We have coffee. And dinosaurs");
+function Changepic()
+{document.getElementById("Dinosaur").src = "hadrosaurus2.png";}
+/*
+Button that changes the photo when pressed
+Enlarge pic when someone puts mouse over it
+Expand about me section when clicked
+*/
